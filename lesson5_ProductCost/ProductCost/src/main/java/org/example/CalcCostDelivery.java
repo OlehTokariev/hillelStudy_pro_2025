6a32bc27b5ed266d6c2d3b371b95c195b86b46d1
OlehTokariev <tokarevolega95@gmail.com>
2025-01-09 19:14:50 +0200

    Added new project for lesson5

---
+++ lesson5_ProductCost/ProductCost/src/main/java/org/example/CalcCostDelivery.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class CalcCostDelivery extends CalcCostBase {
+    private final static double DELIVERY_PRICE = 7.0;
+    @Override
+    public double calcCost(Product product) {
+        return super.calcCost(product) + DELIVERY_PRICE;
+    }
+}
