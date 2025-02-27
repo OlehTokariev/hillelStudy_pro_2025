5fc7b1fa717ea33932525ac30f6ee3e0296ca751
OlehTokariev <tokarevolega95@gmail.com>
2025-02-27 16:48:12 +0200

    Added product analyser for stream pj

---
+++ Product-Analysis/src/app/Product.java
@@ -0,0 +1,30 @@
+package app;
+
+public class Product {
+    private final String name;
+    private final String category;
+    private final double price;
+
+    public Product(String name, String category, double price) {
+        this.name = name;
+        this.category = category;
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", category='" + category + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
