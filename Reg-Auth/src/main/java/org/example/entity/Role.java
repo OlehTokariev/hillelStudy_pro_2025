9937b11f7c802b8ef494f5ab8dd4d9f9ec864ac1
OlehTokariev <tokarevolega95@gmail.com>
2025-06-20 12:16:26 +0300

    less43/Spring Security. Implement user authentication pj using Spring Boot and Bootstrap

---
+++ Reg-Auth/src/main/java/org/example/entity/Role.java
@@ -0,0 +1,21 @@
+package org.example.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
